3fd7514681f9d5ffc6b967b224aa2e59c0932670
jdh0309032 <jdh0309032@gmail.com>
2026-09-13 03:02:51 +0900

    롤백

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
-// index.html에서 이미 Firebase가 초기화되었으므로 바로 database를 연결합니다.
+// 🔥 본인의 Firebase 설정 정보를 입력해주세요! (단 한 번만 선언)
+const firebaseConfig = {
+    apiKey: "AIzaSyCStXmToMtsIBl6cgWqKK1py40CUwYVCH4",
+    authDomain: "pangkah-4ce6e.firebaseapp.com",
+    databaseURL: "https://pangkah-4ce6e-default-rtdb.firebaseio.com",
+    projectId: "pangkah-4ce6e",
+    storageBucket: "pangkah-4ce6e.firebasestorage.app",
+    messagingSenderId: "446419969567",
+    appId: "1:446419969567:web:7fca98d58fefcf4c25972a",
+    measurementId: "G-K7EP7PXFB4"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
 let roomRef = null;
 let myPlayerIndex = 0;
@@ -319,7 +333,7 @@ function createColorizedZodiacTexture(sign, hexColor, callback) {
         ctx.fillRect(0, 0, 256, 256);
         
         let texture = new THREE.CanvasTexture(canvas);
-        texture.colorSpace = THREE.SRGBColorSpace;
+        texture.encoding = THREE.sRGBEncoding;
         zodiacCanvasTextureCache[cacheKey] = texture;
         callback(texture);
     };
@@ -374,7 +388,7 @@ function getTrailTexture() {
     ctx.fillRect(0, 0, 64, 128);
     
     let tex = new THREE.CanvasTexture(canvas);
-    tex.colorSpace = THREE.SRGBColorSpace;
+    tex.encoding = THREE.sRGBEncoding;
     trailTextureCache['default'] = tex;
     return tex;
 }
@@ -875,7 +889,7 @@ gltfLoader.load('./assets/models/custom_arena.glb', (gltf) => {
                         textureLoader.load(`./assets/textures/ads/${fileName}`, (tex) => {
                             tex.wrapS = THREE.ClampToEdgeWrapping;
                             tex.wrapT = THREE.ClampToEdgeWrapping;
-                            tex.colorSpace = THREE.SRGBColorSpace;
+                            tex.encoding = THREE.sRGBEncoding;
 
                             tex.center.set(0.5, 0.5);
                             if (mName.includes('center')) {
@@ -994,13 +1008,13 @@ function triggerDash() {
     player.dashDirZ = (dirZ / len) * 7;
 }
 
-window.handleActionClick = function() {
+function handleActionClick() {
     if (player && !player.isAlive && gameMode === 'private') {
         switchSpectateTarget();
     } else {
         triggerDash();
     }
-};
+}
 
 function switchSpectateTarget() {
     let aliveEntities = entities.filter(e => e.isAlive);
